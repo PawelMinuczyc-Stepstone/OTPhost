206ce3ea2aba5ae1b77e9fcf61e85fa13a5400e6
PawelMinuczyc-Stepstone <52315014+PawelMinuczyc-Stepstone@users.noreply.github.com>
2021-10-05 11:36:51 +0200

    smallfix

--- OTPscript.js
+++ OTPscript.js
@@ -46,10 +46,10 @@ function getNemIDcode(cardID, number) {
 		}
 	};
 
-	// xhttp.open(
-	// 	"GET",
-	// 	"https://otp-nemid.sdc.dk/simulator/listotpcards?serialnumber=" + cardID,
-	// 	true
-	// );
+	xhttp.open(
+		"GET",
+		"https://otp-nemid.sdc.dk/simulator/listotpcards?serialnumber=" + cardID,
+		true
+	);
 	xhttp.send();
 }
